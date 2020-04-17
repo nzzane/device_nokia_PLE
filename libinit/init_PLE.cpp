278afd03c021c792c35e1383cb5b5ccb88f1eb8b
Zane <github@turtledev.nz>
2020-04-17 18:43:56 +1200

    Initial Commit

---
+++ libinit/init_PLE.cpp
@@ -0,0 +1,109 @@
+/*
+   Copyright (c) 2016, The CyanogenMod Project
+   Copyright (c) 2017, The LineageOS Project
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <cstdlib>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <android-base/file.h>
+#include <android-base/properties.h>
+
+#include "init_msm8937.h"
+
+#include "vendor_init.h"
+#include "property_service.h"
+
+using android::base::GetProperty;
+using android::init::property_set;
+
+void check_device()
+{
+    struct sysinfo sys;
+
+
+/*
+ * In some device revisions, there is a sound amplifier that is not activated at
+ * startup.
+ * In stock init binary file, there is mention of the folder, which in theory is
+ * created when you connect the device in the sysfs section.
+ * If this method does not work, replace this piece of code with the working
+ * method or revert it.
+ * If you find a more stable method, then replace it.
+ *
+ * @Author BeYkeRYkt (21-09-2017)
+ */
+void vendor_load_properties()
+{
+    init_alarm_boot_properties();
+
+    check_device();
+    property_set("dalvik.vm.heapstartsize", heapstartsize);
+    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_set("dalvik.vm.heapsize", heapsize);
+    property_set("dalvik.vm.heaptargetutilization", "0.75");
+    property_set("dalvik.vm.heapminfree", heapminfree);
+    property_set("dalvik.vm.heapmaxfree", heapmaxfree);
+
+    property_set("ro.hwui.texture_cache_size", "72");
+    property_set("ro.hwui.layer_cache_size", "48");
+    property_set("ro.hwui.r_buffer_cache_size", "8");
+    property_set("ro.hwui.path_cache_size", "32");
+    property_set("ro.hwui.gradient_cache_size", "1");
+    property_set("ro.hwui.drop_shadow_cache_size", "6");
+    property_set("ro.hwui.texture_cache_flushrate", "0.4");
+    property_set("ro.hwui.text_small_cache_width", "1024");
+    property_set("ro.hwui.text_small_cache_height", "1024");
+    property_set("ro.hwui.text_large_cache_width", "2048");
+    property_set("ro.hwui.text_large_cache_height", large_cache_height);
+
+    std::string cmv = GetProperty("ro.boot.cmv","");
+
+    if (cmv == "mv1")
+    {
+        /* TA-1021 */
+        property_set("ro.sf.lcd_density", "480");
+        property_set("ro.media.maxmem", "10590068224");
+    }
+    else if (cmv == "mv2")
+    {
+        /* TA-1021 */
+        property_set("ro.sf.lcd_density", "480");
+    }
+    else if (cmv == "mv3")
+    {
+        /* TA-1021 */
+        property_set("ro.sf.lcd_density", "480");
+        property_set("persist.bootanimation.scale", "1.5");
+        property_set("sys.lights.capabilities", "3");
+//        property_set("ro.power_profile.override", "power_profile_2x");
+    }
+}
+
