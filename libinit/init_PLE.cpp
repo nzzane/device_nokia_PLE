02bde6d8423e459acc96f470349807c0aa79ec81
Zane <github@turtledev.nz>
2020-04-17 19:38:04 +1200

    remove useless stuff

--- libinit/init_PLE.cpp
+++ libinit/init_PLE.cpp
@@ -46,28 +46,6 @@ using android::init::property_set;
 
 void vendor_load_properties()
 {
-    init_alarm_boot_properties();
-
-    check_device();
-    property_set("dalvik.vm.heapstartsize", heapstartsize);
-    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_set("dalvik.vm.heapsize", heapsize);
-    property_set("dalvik.vm.heaptargetutilization", "0.75");
-    property_set("dalvik.vm.heapminfree", heapminfree);
-    property_set("dalvik.vm.heapmaxfree", heapmaxfree);
-
-    property_set("ro.hwui.texture_cache_size", "72");
-    property_set("ro.hwui.layer_cache_size", "48");
-    property_set("ro.hwui.r_buffer_cache_size", "8");
-    property_set("ro.hwui.path_cache_size", "32");
-    property_set("ro.hwui.gradient_cache_size", "1");
-    property_set("ro.hwui.drop_shadow_cache_size", "6");
-    property_set("ro.hwui.texture_cache_flushrate", "0.4");
-    property_set("ro.hwui.text_small_cache_width", "1024");
-    property_set("ro.hwui.text_small_cache_height", "1024");
-    property_set("ro.hwui.text_large_cache_width", "2048");
-    property_set("ro.hwui.text_large_cache_height", large_cache_height);
-
     std::string cmv = GetProperty("ro.boot.cmv","");
 
     if (cmv == "mv1")
